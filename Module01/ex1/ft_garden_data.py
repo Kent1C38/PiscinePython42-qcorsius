2ba120283d63680065d9e44ce696b3e8302e9b8e
Kent1C8 <corsiusquentin@gmail.com>
2026-01-20 16:03:11 +0100

    Added ex1 (Mod 01)

---
+++ Module01/ex1/ft_garden_data.py
@@ -0,0 +1,22 @@
+from pip._vendor.rich import print
+
+
+class Plant:
+    def __init__(self, name: str, height: int, age: int):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def print_values(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+if __name__ == "__main__":
+    rose = Plant("Rose", 25, 30)
+    sunflower = Plant("Sunflower", 80, 45)
+    cactus = Plant("Cactus", 15, 120)
+
+    print("=== Garden Registry ===")
+    rose.print_values()
+    sunflower.print_values()
+    cactus.print_values()
