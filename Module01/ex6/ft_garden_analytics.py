ace2784c317bd57ea002f86a405a52c26140c13f
Kent1C8 <corsiusquentin@gmail.com>
2026-01-31 13:58:07 +0100

    Added doc strings to ex6 (Mod 1)

--- Module01/ex6/ft_garden_analytics.py
+++ Module01/ex6/ft_garden_analytics.py
@@ -1,3 +1,14 @@
+"""
+Checks if the given object is inherited from the given class
+
+Keyword arguments:
+obj - The object you want to test
+cls - The class that you want to know if obj is inherited
+
+Return value: bool
+"""
+
+
 def check_instance(obj: object, cls: type) -> bool:
     return cls in obj.__class__.__mro__
 
@@ -5,10 +16,28 @@ def check_instance(obj: object, cls: type) -> bool:
 class Plant:
     _types = {}
 
+    """
+    Initializes plants subclasses by registering them with a type name
+
+    Keyword arguments:
+    cls - The class/subclass that inherits from plants you want to make inherit your new subclass
+    Optionnal arguments:
+    type_name - The 'registered' name used in the creation of a plant to specify the class inheritance
+    **kwargs - Options from other classes
+    """
+
     def __init_subclass__(cls, *, type_name=None, **kwargs):
         super().__init_subclass__(**kwargs)
         if type_name:
             Plant._types[type_name] = cls
+
+    """
+    Creates a Plant object
+
+    Keyword arguments:
+    name - The plant's name (positive int)
+    height - The plants's height (positive int)
+    """
 
     def __init__(self, name: str, height: int):
         if Plant.is_height_valid(height):
@@ -17,6 +46,13 @@ class Plant:
         else:
             raise ValueError("Height must be a positive integer !")
 
+    """
+    Increases the height of your plant
+
+    Keyword arguments:
+    size - The size (in cm) your plant must grow (positive int)
+    """
+
     def grow(self, size: int):
         if Plant.is_height_valid(size):
             self.height += size
@@ -24,8 +60,24 @@ class Plant:
         else:
             raise ValueError("Size must be a positive integer !")
 
+    """
+    Retrieves useful infos about your plant
+
+    Return value: str
+    """
+
     def get_infos(self) -> str:
         return f"{self.name}: {self.height}cm"
+
+    """
+    Creates a new plant using the right subclass following the given type name
+
+    Keyword arguments:
+    cls: The current class 
+    type_name: The string representing the class you want it to inherit
+    *args: The arguments that should be given to create your plant
+    **kwargs: Arguments that are consumed by the constructor
+    """
 
     @classmethod
     def create(cls, type_name, *args, **kwargs):
@@ -34,8 +86,14 @@ class Plant:
         except KeyError:
             raise ValueError("Unknown plant type")
 
+    """
+    Checks if the given value is valid for the height value
+
+    Return value: bool
+    """
+
     @staticmethod
-    def is_height_valid(height: int):
+    def is_height_valid(height: int) -> bool:
         return height > 0
 
 
@@ -44,13 +102,30 @@ class BasePlant(Plant, type_name="plant"):
 
 
 class FloweringPlant(Plant, type_name="flower"):
+    """
+    Cretes a Flower object
+
+    Keyworda args:
+    name - The name of your flower
+    height - The name of your flower
+    color - The color of your flower
+    """
+
     def __init__(self, name: str, height: int, color: str):
         super().__init__(name, height)
         self.color = color
         self.blooming = False
 
+    """
+    Define your flower as blooming
+    """
+
     def bloom(self):
         self.blooming = True
+
+    """
+    Override from Plant class
+    """
 
     def get_infos(self) -> str:
         msg = f"{super().get_infos()}, {self.color} flowers"
@@ -60,27 +135,76 @@ class FloweringPlant(Plant, type_name="flower"):
 
 
 class PrizeFlower(FloweringPlant, type_name="prized"):
+    """
+    Creates a PrizeFlower object
+
+    Keyword arguments:
+    name - The name of your flower
+    height - The height of your flower
+    color - The color of your flower
+    prize_points - Points attributed to your flower
+    """
+
     def __init__(self, name: str, height: int, color: str, prize_points: int):
         super().__init__(name, height, color)
         self.prize_points = prize_points
+
+    """
+    Override from FloweringPlant class
+    """
 
     def get_infos(self) -> str:
         return f"{super().get_infos()}, {self.prize_points} prize points"
 
 
 class Garden:
+    """
+    Creates a Garden object
+
+    Keyword arguments:
+    owner - The name of the garden's owner
+    """
+
     def __init__(self, owner: str):
         self.owner = owner
         self.content: dict[str, Plant] = {}
 
+    """
+    Adds a plant in the garden
+
+    Keyword arguments:
+    plant - The plant to add 
+    """
+
     def add_plant(self, plant: Plant):
         self.content[plant.name] = plant
+
+    """
+    Get the plant object from it's name
+
+    Keyword arguments:
+    name - The name of the wated plant
+
+    Return values: The associated Plant object | None if not found
+    """
 
     def get_plant(self, name: str):
         return self.content[name]
 
+    """
+    Increases the height of your plant
+
+    Keyword arguments:
+    name - The name of he plant you want to grow
+    size - The amount it should be increased
+    """
+
     def grow_plant(self, name: str, size: int):
         self.get_plant(name).grow(size)
+
+    """
+    Reports all informations of the garden
+    """
 
     def report(self):
         print(f"=== {self.owner}'s Garden Report ===")
@@ -90,6 +214,10 @@ class Garden:
 
 
 class GardenStats:
+    """
+    Creates a GardenStats object
+    """
+
     def __init__(self):
         self.regular = 0
         self.flowering = 0
@@ -97,6 +225,12 @@ class GardenStats:
         self.total_added = 0
         self.total_growth = 0
         self.garden_count = 0
+
+    """
+    Retrieves the total amount of prize points of all the flowers inside the garden
+
+    Return value: int
+    """
 
     @staticmethod
     def get_total_points(garden: Garden) -> int:
@@ -108,26 +242,71 @@ class GardenStats:
 
 
 class GardenManager:
+    """
+    Creates a GardenManager object
+    """
+
     def __init__(self):
         self.stats = GardenStats()
+
+    """
+    Add a garden that the manager will handle
+
+    Keyword arguments:
+    owner - The name of the new garden's owner
+    """
 
     def add_garden(self, owner: str):
         self.gardens[owner] = Garden(owner)
         self.stats.garden_count += 1
         print(f"{owner} registered their garden")
 
+    """
+    Retrieves the garden owned by the specified person
+
+    Keyword arguments:
+    owner - The name of the owner you want the garden from
+
+    Return value: Garden
+    """
+
     def get_garden(self, owner: str) -> Garden:
         return self.gardens.get(owner)
+
+    """
+    Increases the specified plant height from a person's garden
+
+    Keyword arguments:
+    owner - The owner's name
+    name - The plant's name
+    size - The amount the height must be incremented
+    """
 
     def grow_plant(self, owner: str, name: str, size: int):
         self.get_garden(owner).get_plant(name).grow()
         self.stats.total_growth += size
+
+    """
+    Grows all plants from someone's garden
+
+    Keyword arguments:
+    owner - The owner's name
+    size - The amount all plants must grow
+    """
 
     def grow_all_plants(self, owner: str, size: int):
         print(f"{owner} is helping all the plants from their garden to grow...")
         for plant in self.get_garden(owner).content.values():
             plant.grow(size)
             self.stats.total_growth += size
+
+    """
+    Adds a plant in someone's garden
+
+    Keyword arguments:
+    owner - The name of the person that own the garden the plant shoul be added to 
+    plant - The plant that is going to be added
+    """
 
     def add_plant_to_garden(self, owner: str, plant: Plant):
         if self.get_garden(owner) is None:
@@ -141,6 +320,10 @@ class GardenManager:
         else:
             self.stats.regular += 1
         print(f"Added {plant.name} to {owner}'s Garden")
+
+    """
+    Shows all the statistics tracked by GardenStats
+    """
 
     def show_stats(self):
         scores = ""
@@ -162,6 +345,13 @@ class GardenManager:
         print(f"Total managed gardens: {self.stats.garden_count}")
 
 
+"""
+Creates the GardenManager fully initialized
+
+Return value: GardenManager
+"""
+
+
 def create_garden_network() -> GardenManager:
     manager = GardenManager()
     manager.gardens: dict[str, Garden] = {}
@@ -174,7 +364,6 @@ if __name__ == "__main__":
 
     manager.add_garden("Alice")
     manager.add_garden("Bob")
-
     print()
     manager.add_plant_to_garden("Alice", Plant.create("plant", "Oak tree", 101))
     manager.add_plant_to_garden("Alice", Plant.create("flower", "Rose", 26, "red"))
