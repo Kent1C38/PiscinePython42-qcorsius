4a95fc5290ed9a02c795a7e20c1737b1b03e0757
Kent1C8 <corsiusquentin@gmail.com>
2026-02-03 11:59:01 +0100

    Added ex0 (Mod 3)

---
+++ Module03/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    if len(sys.argv) < 1:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments recieved: {len(sys.argv) - 1}")
+        counter = 1;
+        for arg in sys.argv:
+            if arg == sys.argv[0]:
+                pass
+            else:
+                print(f"Argument {counter}: {arg}")
+                counter += 1
+    print(f"Total arguments: {len(sys.argv)}")
