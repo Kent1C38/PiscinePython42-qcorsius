0bb90fa65972c0ec3b1975518c3f74973801c8e6
Kent1C38 <corsiusquentin@gmail.com>
2026-04-01 08:29:21 +0200

    Finalized ex3 Module10

--- Module10/ex3/functools_artifacts.py
+++ Module10/ex3/functools_artifacts.py
@@ -1,4 +1,4 @@
-from functools import reduce, partial, lru_cache
+from functools import reduce, partial, lru_cache, singledispatch
 
 
 def spell_reducer(spells: list[int], operation: str) -> int:
@@ -32,17 +32,69 @@ def partial_enchanter(base_enchantment: callable) -> dict[str, callable]:
 
 
 @lru_cache
-def memoized_fibonacci(n):
+def memoized_fibonacci(n: int):
+    if n < 2 and n >= 0:
+        return n
+    elif n < 0:
+        return 0
+
+    return memoized_fibonacci(n - 1) + memoized_fibonacci(n - 2)
+
+
+def spell_dispactcher() -> callable:
+
+    @singledispatch
+    def cast(spell):
+        return f"Unknown Spell type: {type(spell)}"
+
+    @cast.register(str)
+    def _(spell):
+        return f"Casting Enchantment: {spell}"
+
+    @cast.register(int)
+    def _(spell):
+        return f"Casting Damage Spell: {spell} power"
+
+    @cast.register(list)
+    def _(spell):
+        results = [cast(s) for s in spell]
+        return "Multi-cast:\n" + "\n".join(results)
+
+    return cast
 
 
 if __name__ == "__main__":
+    print("="*5 + "Reducer demo" + "="*5)
     print(spell_reducer([1, 2, 3, 4], "add"))
     print(spell_reducer([1, 2, 3, 4], "multiply"))
     print(spell_reducer([1, 2, 3, 4], "max"))
     print(spell_reducer([1, 2, 3, 4], "min"))
+
+    print("\n" + "="*5 + "Partial completing demo" + "="*5)
 
     def enchant(power: int, element: str, target) -> str:
         return f"Target: {target}, spell: {element} ({power} power)"
 
     enchanter = partial_enchanter(enchant)
     print(enchanter["fire_enchant"]("test")())
+
+    print("\n" + "="*5 + "LRU Cache demo" + "="*5)
+    from time import perf_counter
+    start = perf_counter()
+    print(memoized_fibonacci(15), end="")
+    print(f" ({perf_counter() - start: 0.6f}s elapsed)")
+
+    start = perf_counter()
+    print(memoized_fibonacci(30), end="")
+    print(f" ({perf_counter() - start: 0.6f}s elapsed)")
+
+    start = perf_counter()
+    print(memoized_fibonacci(50), end="")
+    print(f" ({perf_counter() - start: 0.6f}s elapsed)")
+
+    print("\n" + "="*5 + "Dispatcher demo" + "="*5)
+
+    dispatch = spell_dispactcher()
+    print(dispatch("test"))
+    print(dispatch(50))
+    print(dispatch(["re-test", 40, lambda: "normalement ça marche pas"]))
