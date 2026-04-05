f87e9f4167b8a9558d0796fc9dbcb3ef3e55daa5
Kent1C38 <corsiusquentin@gmail.com>
2026-04-05 18:44:08 +0200

    Started modifications on Module10 up to ex1

--- Module10/ex1/higher_magic.py
+++ Module10/ex1/higher_magic.py
@@ -4,33 +4,52 @@ from typing import Callable
 def spell_combiner(spell1: callable, spell2: callable) -> callable:
     if not isinstance(spell1, Callable) or not isinstance(spell2, Callable):
         return lambda: None
-    return lambda x: (spell1(x), spell2(x))
+    return lambda target, power: (spell1(target, power), spell2(target, power))
 
 
 def power_amplifier(base_spell: callable, multiplier: int) -> callable:
     if not isinstance(base_spell, Callable):
         return lambda: None
-    return lambda: base_spell() * multiplier
+    return lambda target, power: base_spell(target, power * multiplier)
 
 
 def conditional_caster(condition: callable, spell: callable) -> callable:
     if not isinstance(condition, Callable) or not isinstance(spell, Callable):
         return lambda: None
-    return lambda x: spell(x) if condition(x) else "Spell fizzled"
+    return lambda target, power: (spell(target, power)
+                                  if condition(target, power)
+                                  else "Spell fizzled")
 
 
 def spell_sequence(spells: list[callable]) -> callable:
-    return lambda x: [spell(x) for spell in spells]
+    return lambda target, power: [spell(target, power) for spell in spells]
 
 
 if __name__ == "__main__":
-    print(*spell_combiner(lambda x: f"Hit {x}",
-                          lambda x: f"Heal {x}")("Dragon"))
 
-    print(power_amplifier(lambda: "Fireball ", 5)())
+    def fireball(target: str, power: int):
+        return f"Fireball hits {target} and deals {power} damage"
 
-    print(conditional_caster(
-        lambda x: len(x) <= 15, lambda x: f"Cast {x}")("fireball"))
+    def heal(target: str, power: int):
+        return f"Healing {target} for {power} HP"
 
-    print(spell_sequence([lambda x: f"Fireball on {x}",
-                          lambda x: f"Ice spike on {x}"])("Dragon"))
+    def is_spell_valid(target: str, power: int):
+        return len(target) > 3 and power > 0
+
+    print(spell_combiner(fireball, heal)("Dragon", 5))
+
+    print()
+
+    mega_fireball = power_amplifier(fireball, 3)
+    print(mega_fireball("Snowman", 5))
+
+    print()
+
+    spell_cast = conditional_caster(is_spell_valid, heal)
+    print(spell_cast("Dummy", 0))
+    print(spell_cast("Dummy", 5))
+
+    print()
+
+    sequence = spell_sequence([fireball, heal])
+    print(sequence("test", 8))
