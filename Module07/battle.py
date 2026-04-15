d69fa3dd740eb99a9e91ef97060f17b24fcd2e26
Kent1C38 <corsiusquentin@gmail.com>
2026-04-15 18:11:07 +0200

    Patched battle.py file to respect the subject

--- Module07/battle.py
+++ Module07/battle.py
@@ -1,27 +1,36 @@
 import ex0
+from ex0.AbstractFactory import CreatureFactory
+
+
+def check_creaure_creation(factory: CreatureFactory):
+    print("Testing factory...")
+    base = factory.create_base()
+    evolved = factory.create_evolved()
+    print(base.describe())
+    print(base.attack())
+    print(evolved.describe())
+    print(evolved.attack())
+
+
+def battle_base_test(fac1: CreatureFactory, fac2: CreatureFactory):
+    print("Testing battle...")
+    base1 = fac1.create_base()
+    base2 = fac2.create_base()
+    print(f"{base1.describe()}\n vs\n{base2.describe()}\n fight!")
+    print(base1.attack())
+    print(base2.attack())
+
 
 if __name__ == "__main__":
     fire_fac = ex0.FlameFactory()
     water_fac = ex0.AquaFactory()
 
-    print("Testing factory")
-    flameling = fire_fac.create_base()
-    pyrodon = fire_fac.create_evolved()
-    print(flameling.describe())
-    print(flameling.attack())
-    print(pyrodon.describe())
-    print(pyrodon.attack())
+    check_creaure_creation(fire_fac)
 
-    print("\nTesting factory")
-    aquabub = water_fac.create_base()
-    torragon = water_fac.create_evolved()
-    print(aquabub.describe())
-    print(aquabub.attack())
-    print(torragon.describe())
-    print(torragon.attack())
+    print()
 
-    print("\nTesting Battle")
-    print(f"{flameling.describe()}\n vs\n{aquabub.describe()}")
-    print(" fight!")
-    print(flameling.attack())
-    print(aquabub.attack())
+    check_creaure_creation(water_fac)
+
+    print()
+
+    battle_base_test(fire_fac, water_fac)
